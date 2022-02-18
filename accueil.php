c1d4b8129dfd7e9d3bfa66119f9cfc0b848b96f8
Cesara02 <90842096+Cesara02@users.noreply.github.com>
2022-02-18 17:04:23 +0100

    Add files via upload

--- accueil.php
+++ accueil.php
@@ -7,44 +7,74 @@
     <link rel="icon" href="">
 </head>
 <body> 
+    <!-- Création du header -->
     <div class="header"> 
         <img src="assets/logo.png" width="120" alt="logo">
     </div>
 
     <div class="affiche"> 
         <?php 
+        //Déclaration de la variable Crud
+        $Crud = '0';
+        //On récupére les données du formulaire
             if(isset($_POST["FilmSubmit"])){
-                if(!empty($_POST["Nom"]) && !empty($_POST["Genre"]) && !empty($_POST["Note"])){
+                //Verification du contenu des champs
+                if(!empty($_POST["Nom"])){
+                    //Insertion des champs dans les tables
                 $Crud = 'C';
                 $NomFilm = $_POST["Nom"];
-                $Genre = $_POST["Genre"];
-                $Note = $_POST["Note"];
                 }
             }
-
+            //Connexion a la BDD avec PDO
         if($Crud!='0') {
             try {
+                //Le PDO attend des paramétre comme le nom de la base , le user avec son mot de pass
                 $MaBase = new PDO('mysql:host=mysql-bordrezcesar.alwaysdata.net; dbname=bordrezcesar_film', '256339_elliot', 'bordrez0908cesar2207');
-
+                //Si la connexion est vrai alors on continue
                 if($MaBase){
+                    //Switch pour gérer les requettes
                     switch ($Crud){
                         case 'C':
                             echo "Ajout d'un film";
-                            $req = "INSERT INTO `FILM` (`nom` `genre` `note`) VALUES ('".$NomFilm."','".$Genre."'.'".$Note."')";
+                            //On crée la requête
+                            $req = "INSERT INTO `FILM` (`nom`) VALUES ('".$NomFilm."')";
                             $RequetStatement = $MaBase->query($req);
-
+                            //Vérification du statement
                             if($RequetStatement){
+                                //Si la BDD répond '00000' alors c'est ok
                                 if($RequetStatement->errorCode()=='00000'){
                                     echo "Film ajouté avec succès ! ";
+                                    echo $NomFilm. "est le nouveau film.";
+                                }else{
+                                    echo "Erreur N°".$RequestStatement->errorCode()." lors de l'insertion";
                                 }
+                            }else{
+                                echo "Erreur dans le format de la requête";
                             }
-                catch(Exception $e){
-                    $e ->getMessage();
+
+                            break;
+                        case 'R':
+                            echo "Vous avez fait une selection de film";
+                            break;
+                        case 'U':
+                            echo "Vous avez fait une update de film";
+                            break;
+                        case 'D':
+                            echo "Vous avez fait une suppression de film";
+                            break;
+                        default:
+                            echo "Vous n'avez pas fait de requête CRUD";
+                            break;
+                        }
                 }
+            }catch(Exception $e){
+                $e->getMessage();
             }
+        }
         ?>
+        <!-- On va maintenant afficher le formulaire pour inserer un film -->
         <form action="" method="post">
-        <label for="Nom">Nom fu film </label>
+        <label for="Nom">Nom du film </label>
         <input type="text" name="Nom" id="Nom" required>
 
         <input type="submit" name="FilmSubmit" value="Ajouter">
--- index.php
+++ index.php
@@ -8,18 +8,23 @@
 </head>
 <body>
     <?php
+    //Traitement du formulaire de connexion
         if(isset($_POST["Valider"])){
         if($_POST["password"]=="root" && $_POST["login"]=="root" ){
+            //Si le login est le mdp est bon alors la connexion est valide
             $_SESSION["EtatConnexion"]=true;
+            //On envoie l'utlisateur vers le site Web
             header('Location: accueil.php');
         }      
     }
     ?>
+    <!-- Page de connexion avec formulaire -->
+    <!-- Login: root Password: root -->
     <div id="container">
     <form action="" method="post">
         <h1>Connexion</h1>
         <label><b>Nom d'utilisateur</b></label>
-        <input type="text" placeholder="Entrer le nom d'utilisateur" name="login" id="login" required>
+        <input type="text" placeholder="Entrer le nom d'utilisateur" name="login" id="login" required> 
 
         <label><b>Mot de passe</b></label>
         <input type="password" placeholder="Entrer le mot de passe" name="password" id="password" required>
